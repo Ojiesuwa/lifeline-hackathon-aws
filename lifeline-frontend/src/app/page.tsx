bab771c7a0d8e314be001ee85ec27928996cdf12
Ojiesuwa <oluwarotimiadeola@gmail.com>
2026-09-14 20:29:11 +0100

    Changes

--- lifeline-frontend/src/app/page.tsx
+++ lifeline-frontend/src/app/page.tsx
@@ -45,6 +45,7 @@ const formatDuration = (durationMs: number) => {
   const totalSeconds = Math.round(durationMs / 1000);
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
+  
 
   if (minutes === 0) {
     return `${seconds}s`;
